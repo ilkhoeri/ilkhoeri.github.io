7846329baee7f2fef2860d02233c5aecddff3c3e
ilkhoeri <khoeriilham@gmail.com>
2025-01-30 00:29:52 +0700

    generateStaticParams

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import { withContentlayer } from "next-contentlayer2";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // must be exported function "generateStaticParams()", which is required with "output: export" config
   reactStrictMode: true,
   experimental: {
     // appDir: true,
