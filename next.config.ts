c4a512b56c92953bf88bb231ef23ca91163c4579
ilkhoeri <khoeriilham@gmail.com>
2025-01-30 00:19:23 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,30 @@ import { withContentlayer } from "next-contentlayer2";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   experimental: {
     // appDir: true,
+  },
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"]
+    });
+    if (!isServer) {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          ...config.resolve.fallback,
+          fs: false,
+          child_process: false,
+          worker_threads: false,
+          "builtin-modules": false
+        }
+      };
+    }
+
+    return config;
   }
 };
 
